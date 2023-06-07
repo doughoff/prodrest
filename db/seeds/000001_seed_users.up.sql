71fe3a069e8d05df07fa3d1bfa90741e0eaa37c5
hoffax <douglashoffmann@outlook.es>
2023-06-07 01:30:12 -0400

    feat: better validation for ruc & ci

--- db/seeds/000001_seed_users.up.sql
+++ db/seeds/000001_seed_users.up.sql
@@ -6,3 +6,4 @@ INSERT INTO
     public.users (id, status, email, name, password, roles)
 VALUES
     ('d3af6323-2e9a-4762-89aa-d65aefa82b55', 'ACTIVE', 'operator@hk.com', 'operator', '123456', '{}');
+
